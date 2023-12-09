88afc0252fee9de5daec18339d2e56e91e385c99
Boris_Razor <boria2008@inbox.ru>
2023-12-09 23:03:24 +0300

    Added a solution to the third problem

--- task_03/Program.cs
+++ task_03/Program.cs
@@ -3,3 +3,24 @@
 // Задача 3: Задайте произвольную строку. 
 //Выясните, является ли она палиндромом.
 
+bool StringPalindrom(string str)
+{
+    while (true)
+    {
+        for (int i = 0, j = str.Length - 1; i < str.Length / 2; i++, j--)
+        {
+            if (str[i] == str[j])
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
+Console.WriteLine("Enter a string");
+if (StringPalindrom(Console.ReadLine()))
+{
+    Console.WriteLine("Palindrom");
+}
+else Console.WriteLine("Not palindrom");
