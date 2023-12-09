487d62252007c80e24016e74e359a950d9d0f7f6
Boris_Razor <boria2008@inbox.ru>
2023-12-09 21:57:07 +0300

    added a solution to the second problem

--- task_02/Program.cs
+++ task_02/Program.cs
@@ -2,3 +2,19 @@
 
 // Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах.
 // Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+string str = "BoRiS_RaZoR";
+
+string FromUpperToLower(string str)
+{
+    string newStr = String.Empty;
+    foreach (char item in str)
+    {
+        newStr += char.ToLower(item);
+    }
+    return newStr;
+}
+
+Console.WriteLine(str);
+Console.WriteLine(
+    FromUpperToLower(str));
